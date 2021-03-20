3213c164b660f4f4c6308835515de0877115507f
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-03-20 16:22:18 -0300

    Create mb.py

---
+++ mb.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+for i in range(0,10):
+  data=$"msisdn=79998081034&campid=4919b47c-f588-4e71-87e3-639b3af92e4d&opCode=VV" && site=$"http://interatividade.vivo.ddivulga.com/carrotProcess" && curl "${site}" -d "${data}"
+  done
