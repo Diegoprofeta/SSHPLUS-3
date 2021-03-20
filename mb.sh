aa8420292a649eda55ad3b00a9aa07df3e79a199
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-03-20 17:43:52 -0300

    Update mb.sh

--- mb.sh
+++ mb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+clear
 echo "INFORME SEU NÚMERO (DDD+NÚMERO)"
 read SEU NUMERO
 echo "QUANTIDADE DE VEZES (1GB = 20VEZES)"
 read VEZES
 for i in $(seq $VEZES);
 do
+clear
 data=$"msisdn=$SEU NUMERO&campid=4919b47c-f588-4e71-87e3-639b3af92e4d&opCode=VV" && site=$"http://interatividade.vivo.ddivulga.com/carrotProcess" && curl "${site}" -d "${data}"
 echo $i;
 done
