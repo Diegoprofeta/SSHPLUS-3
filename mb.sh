100cb40e6626d1c06a875b0df128655fa25b2cfa
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-03-20 17:54:07 -0300

    Update mb.sh

--- mb.sh
+++ mb.sh
@@ -7,5 +7,5 @@ read VEZES
 for i in $(seq $VEZES);
 do
 data=$"msisdn=$SEU NUMERO&campid=4919b47c-f588-4e71-87e3-639b3af92e4d&opCode=VV" && site=$"http://interatividade.vivo.ddivulga.com/carrotProcess" && curl "${site}" -d "${data}"
-echo \n$i;
+echo -e \n$i;
 done
