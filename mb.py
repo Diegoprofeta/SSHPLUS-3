3eb0c3918a4392ede10098e83eb036aea9e6322f
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-03-20 17:36:09 -0300

    Update and rename mb.py to mb.sh

--- mb.py
+++
@@ -1,4 +0,0 @@
-for i in range(0,10):
-  try:
-  	data=$"msisdn=79998081034&campid=4919b47c-f588-4e71-87e3-639b3af92e4d&opCode=VV" && site=$"http://interatividade.vivo.ddivulga.com/carrotProcess" && curl "${site}" -d "${data}"
-break
