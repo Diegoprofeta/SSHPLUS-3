f88d3937d0ed7dec559e107527424c4eaae9ef46
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-08-13 20:35:45 -0300

    Update wsproxy.py

--- Modulos/wsproxy.py
+++ Modulos/wsproxy.py
@@ -10,8 +10,11 @@ except:
    LISTENING_PORT = 80
 BUFLEN = 4096 * 4
 TIMEOUT = 60
+MSG = ''
+COR = '<font color="null">'
+FTAG = '</font>'
 DEFAULT_HOST = "127.0.0.1:22"
-RESPONSE = 'HTTP/1.1 101 CONECT4G \r\n\r\n'
+RESPONSE = "HTTP/1.1 101 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
  
 class Server(threading.Thread):
     def __init__(self, host, port):
