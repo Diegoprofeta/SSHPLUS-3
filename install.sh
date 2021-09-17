664c34fb2efd332ec1f6bc9879744a2f1b9d6b43
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-09-17 00:39:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
-#===============================#
-# \033[1;36m
-# \033[1;33m
-# \033[1;31m
-# \033[0m
-#===============================#
 
-clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;31mINSTALADOR DO SITE BY:LUIZ\033[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+install_fun () {
+    clear
+    echo -e "ATUALIZANDO REPOSITORIO... \033[1;32mAGUARDE"
+    apt-get update -y > /dev/null 2>&1
+    apt-get upgrade -y > /dev/null 2>&1
+    apt-get install curl -y > /dev/null 2>&1
+    apt-get install apache2 -y > /dev/null 2>&1
+    sed -i "s;Listen 81;Listen 80;g" /etc/apache2/ports.conf
+    service apache2 restart > /dev/null 2>&1
+    echo -e " "
+    echo -e "\033[1;33m DONWLOAD SERVER... \033[1;32mAGUARDE"
+    apt-get install zip -y > /dev/null 2>&1
+    apt-get install unzip -y > /dev/null 2>&1
+    apt-get install curl -y > /dev/null 2>&1
+    [[ ! -d /var/www/html/_S_script ]] && mkdir /var/www/html/_S_script
+    cd /var/www/html/_S_script
+    wget https://www.dropbox.com/s/lpexscdu1l1gw7a/teste.zip > /dev/null 2>&1
+    unzip teste.zip > /dev/null 2>&1
+    rm -rf teste.zip
+    cd
+    echo -e " "
+    cd
+    rm install > /dev/null 2>&1
+    echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo -e " Site migrado com sucesso..."
+    echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+}
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "INSTALADOR DO SITE BY:LUIZ"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo -ne "\033[0m"
-echo -e "\033[1;33mINSTALANDO... \033[1;32mAGUARDE"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "INSTALANDO... AGUARDE"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo -e " "
-# ACTUALIZA REPOSITORIO
-echo -e "\033[1;33m ATUALIZANDO REPOSITORIO... \033[1;32mAGUARDE"
-apt-get update -y > /dev/null 2>&1
-apt-get upgrade -y > /dev/null 2>&1
-apt-get install curl -y > /dev/null 2>&1
-apt-get install apache2 -y > /dev/null 2>&1
-sed -i "s;Listen 81;Listen 80;g" /etc/apache2/ports.conf
-service apache2 restart > /dev/null 2>&1
-cat /dev/null > ~/.bash_history && history -c
-echo -e " "
-# DONWLOAD SERVER
-echo -e "\033[1;33m DONWLOAD SERVER... \033[1;32mAGUARDE"
-#apt-get install zip -y > /dev/null 2>&1
-apt-get install unzip -y > /dev/null 2>&1
-apt-get install curl -y > /dev/null 2>&1
-[[ ! -d /var/www/html/_S_script ]] && mkdir /var/www/html/_S_script
-cd /var/www/html/_S_script
-wget https://www.dropbox.com/s/lpexscdu1l1gw7a/teste.zip > /dev/null 2>&1
-unzip teste.zip > /dev/null 2>&1
-rm -rf teste.zip
-cd
-# HISTORY
-cat /dev/null > ~/.bash_history && history -c
-echo -e " "
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;37m Site migrado com sucesso..."
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-cd
-rm install.sh > /dev/null 2>&1
-
+install_fun
