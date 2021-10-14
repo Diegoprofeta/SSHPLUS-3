292e8d9a0a74db6315b0571d4a1f1e6dbb6b1742
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-10-14 00:02:05 -0300

    Update wsproxy.py

--- wsproxy.py
+++ wsproxy.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
+ cat <<EOF > proxy.py
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # CONFIG
 LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = 1080
+try:
+   LISTENING_PORT = int(sys.argv[1])
+except:
+   LISTENING_PORT = 80
 PASS = ''
-
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
+MSG = ''
+COR = '<font color="null">'
+FTAG = '</font>'
 DEFAULT_HOST = "127.0.0.1:22"
 RESPONSE = "HTTP/1.1 101 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
 
