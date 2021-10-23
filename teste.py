119a02e20b17cdd8c0ffc309f0d1cae9cb5cf05c
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-10-23 17:00:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,22 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # encoding: utf-8
 import socket, threading, thread, select, signal, sys, time, getopt
 
-# Listen
-LISTENING_ADDR = '0.0.0.0'
-if sys.argv[1:]:
-  LISTENING_PORT = sys.argv[1]
-else:
-  LISTENING_PORT = 80  
-#Pass
 PASS = ''
-
-# CONST
+LISTENING_ADDR = '0.0.0.0'
+try:
+   LISTENING_PORT = int(sys.argv[1])
+except:
+   LISTENING_PORT = 80
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:443'
-RESPONSE = ''
-#RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
+MSG = ''
+COR = '<font color="null">'
+FTAG = '</font>'
+DEFAULT_HOST = "127.0.0.1:22"
+RESPONSE = "HTTP/1.1 101 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
 
 class Server(threading.Thread):
     def __init__(self, host, port):
