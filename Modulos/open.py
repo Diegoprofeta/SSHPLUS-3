c45e6cb81d85a8d485e748814ad744acb11c636c
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-11-06 00:22:47 -0300

    Update open.py

--- Modulos/open.py
+++ Modulos/open.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# SSHPLUS By @Crazy_vpn
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
@@ -13,7 +12,7 @@ except:
 PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
-MSG = 'SSHPLUS'
+MSG = 'ALERT'
 DEFAULT_HOST = '0.0.0.0:1194'
 RESPONSE = "HTTP/1.1 101 " + str(MSG) + "\r\n\r\n"
 
