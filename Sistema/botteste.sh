8f37af5b8529a5caa2fc031fae48e55d80c27228
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2021-11-22 16:27:44 -0300

    Add files via upload

--- Sistema/botteste.sh
+++ Sistema/botteste.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo  -e "Imforme o token"
-read token
-cd $HOME/BOT
-screen -dmS bot_teste ./botssh $token > /dev/null 2>&1
+[[ $(screen -list| grep -c 'bot_teste') == '0' ]] && {
+    echo  -e "Imforme o token"
+    read token
+    cd $HOME/BOT
+    screen -dmS bot_teste ./botssh $token > /dev/null 2>&1
+} || {
+    screen -r -S "bot_teste" -X quit
+    clear && echo "BOT DESATIVADO"
+}
